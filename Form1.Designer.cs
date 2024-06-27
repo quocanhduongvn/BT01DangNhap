ef866f182bee4fe06d41433e80e660c65b3b922e
Dương Quốc Anh <percyfire777@gmail.com>
2024-06-28 00:42:23 +0700

    blue

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -49,7 +49,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.ForeColor = System.Drawing.Color.Blue;
             this.label1.Location = new System.Drawing.Point(32, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(124, 15);
@@ -74,6 +74,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
             this.label2.Location = new System.Drawing.Point(32, 44);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(93, 15);
@@ -83,7 +84,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
             this.label3.Location = new System.Drawing.Point(32, 76);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 15);
